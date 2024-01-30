0e59312a6195e15a1c29807249dcf568e4089be4
whoiseon <whois__@naver.com>
2024-01-30 13:04:06 +0900

    feat: protected auth page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,10 +1,5 @@
-import { headers } from 'next/headers';
-
-async function Sign() {
-  const header = headers();
-  const ip = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
-
-  return <div>{ip}</div>;
+function Sign() {
+  return <div>123</div>;
 }
 
 export default Sign;
