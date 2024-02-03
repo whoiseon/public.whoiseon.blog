dd2eacf14f8a069c7272b0a6d655a3dec3ae4277
whoiseon <whois__@naver.com>
2024-02-03 17:20:39 +0900

    fix: cors

--- src/app/api/files/upload/route.ts
+++ src/app/api/files/upload/route.ts
@@ -14,7 +14,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
         error: 'No file received.',
         path: null,
       },
-      { status: 201 },
+      {
+        status: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      },
     );
   }
 
@@ -40,7 +47,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
         error: '',
         path: `https://imslow.me/images/blog/post/${uuid}/${fileName}`,
       },
-      { status: 201 },
+      {
+        status: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      },
     );
   } catch (e) {
     console.log('error', e);
@@ -49,34 +63,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
         error: 'Failed',
         path: null,
       },
-      { status: 201 },
+      {
+        status: 201,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      },
     );
   }
 }
-
-// import { NextResponse } from "next/server";
-// import path from "path";
-// import { writeFile } from "fs/promises";
-//
-// export const POST = async (req, res) => {
-//   const formData = await req.formData();
-//
-//   const file = formData.get("file");
-//   if (!file) {
-//     return NextResponse.json({ error: "No files received." }, { status: 400 });
-//   }
-//
-//   const buffer = Buffer.from(await file.arrayBuffer());
-//   const filename =  file.name.replaceAll(" ", "_");
-//   console.log(filename);
-//   try {
-//     await writeFile(
-//       path.join(process.cwd(), "public/assets/" + filename),
-//       buffer
-//     );
-//     return NextResponse.json({ Message: "Success", status: 201 });
-//   } catch (error) {
-//     console.log("Error occured ", error);
-//     return NextResponse.json({ Message: "Failed", status: 500 });
-//   }
-// };
