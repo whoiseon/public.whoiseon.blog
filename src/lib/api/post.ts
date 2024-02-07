6b7366fb65b14395d8207491ac346fb23b28db3a
whoiseon <whois__@naver.com>
2024-02-07 15:55:56 +0900

    temp: temp post list check 4 & api url

--- src/lib/api/post.ts
+++ src/lib/api/post.ts
@@ -27,7 +27,7 @@ export async function getPostById(postId: number) {
 
 export async function getTempPosts() {
   try {
-    const response = await fetch('http://localhost:3000/api/post/temp', {
+    const response = await fetch('https://imslow.me/api/post/temp', {
       method: 'GET',
       cache: 'no-store',
     });
